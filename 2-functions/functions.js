1a0de253a6f0b68fd9f5f11e70a12148171e956c
Leahkerubo <leahkerubo21@gmail.com>
2019-11-08 08:32:32 +0300

    fixed test 5

--- 2-functions/functions.js
+++ 2-functions/functions.js
@@ -96,7 +96,7 @@ describe("functions", function() {
             // and 'answerThirdResult' with the correct results.
             // If you call getContext() here, you're a cheater! :)
             // ✏️ ADD CODE HERE ✏️
-
+            
             ////////////////////////////////////////////////////////////////////
 
             // First we call the function without an explicit context
